8bb042cd38fe9df9467eae397e7f9a69d3a622f2
chenWiggin <41045731+chenWiggin@users.noreply.github.com>
2019-12-10 09:45:29 +0800

    支持传入{content:,code:}形式的实体类，选择成功返回code

---
+++ android/src/main/java/com/beefe/picker/view/ReturnData.java
@@ -0,0 +1,37 @@
+package com.beefe.picker.view;
+
+/**
+ * Created by shexiaoheng on 2016/12/23.
+ */
+
+public class ReturnData {
+
+    private String item;
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    private int index;
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
